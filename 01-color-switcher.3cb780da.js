0a3ba658e0a190923f4ecfb2fff397017bbd7a7e
NicolayRusetsky <96424328+NicolayRusetsky@users.noreply.github.com>
2023-03-23 11:12:50 +0000

    Deploying to gh-pages from @ NicolayRusetsky/goit-js-hw-09@1ee3dc799ce4dabfadd188ce0f76b9bc057614d4 🚀

---
+++ 01-color-switcher.3cb780da.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body"),d=null;e.setAttribute("disabled","true"),t.addEventListener("click",(function(){t.setAttribute("disabled","true"),e.removeAttribute("disabled"),d=setInterval((function(){r.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){clearInterval(d),t.removeAttribute("disabled"),e.setAttribute("disabled","true")}))}();
+//# sourceMappingURL=01-color-switcher.3cb780da.js.map
